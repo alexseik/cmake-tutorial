a8b207404b97e782e43fe0eae65d4906e6bcbb88
Alejandro Garrido López <Alejandro.Garrido@eurotaxglass.com>
2015-01-30 13:40:37 +0100

    commented rules on etx2sql.h

--- src/etx2Sql.cpp
+++ src/etx2Sql.cpp
@@ -1,5 +1,16 @@
 #include "etx2Sql.h"
 
+/**
+* descfile : descriptor file
+* dirs : directories where find database and dlls
+* outfile : name of file containing the result / the result into a char* variable
+* tmpfile : temporary file for logs
+* deffile : Definition / Queries file. 
+* queryName : name of query launched
+* dynQuery : A string containing a dynamic query to be launched
+* queryPars : Parameters to pass to query/dynQuery
+* nrep : number of results
+*/
 EXTERNAL
 ETX2SQL_EXPORT ETXLONG QueryETXKernel  (const char& descfile, const char& dirs,   
                           char& outfile,const char& tmpfile,
@@ -8,4 +19,10 @@ ETX2SQL_EXPORT ETXLONG QueryETXKernel  (const char& descfile, const char& dirs,
                           long& nrecp)
 {
 	return 0;
+	//TODO : Argument parser
+	//TODO : file reader/writer
+	//TODO : output formatter
+	//TODO : properties reader
+	//TODO : log system
+	//TODO : exception system ??'
 }
